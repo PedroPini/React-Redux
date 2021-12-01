320f840774964dbdaa5bf795d1f285fcf8a4ad81
pedro <pedropini@raptortech.com.au>
2021-12-01 08:38:20 +0800

    Changing redux thunk for saga

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,12 +4,20 @@ export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload };
 }
 
+
+//Redux thunk
+// export function getData() {
+//     return function(dispatch) {
+//       return fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(response => response.json())
+//         .then(json => {
+//           dispatch({ type: DATA_LOADED, payload: json });
+//         });
+//     };
+//   }
+
+
+//Redux Saga
 export function getData() {
-    return function(dispatch) {
-      return fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => {
-          dispatch({ type: DATA_LOADED, payload: json });
-        });
-    };
-  }
+    return { type: "DATA_REQUESTED" };
+}
